37821dac1a81589d0a7a44474d8ea5f71f71c70e
Hassan Nazar <hassan.nazar@danielwellington.com>
2020-09-20 17:16:49 +0200

    quarkus function created

---
+++ faas-runtime-test/faas-test-quarkus/src/main/java/io/hreem/studies/GreetingLambda.java
@@ -0,0 +1,28 @@
+package io.hreem.studies;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import lombok.val;
+
+public class GreetingLambda implements RequestHandler<GreetingInput, Greeting> {
+
+    @Inject
+    NumberGenerator numberGenerator;
+
+    @Override
+    public Greeting handleRequest(GreetingInput input, Context context) {
+        double magicNumber = numberGenerator.generateNumber();
+        val greeting = new Greeting();
+
+        val greetingBuilder = new StringBuilder("Hello ");
+        val recipientName = input == null || input.getName() == null || input.getName().isEmpty() ? 
+            "Stuart" : input.getName();
+        greetingBuilder.append(recipientName);
+        greeting.setGreetingMessage(greetingBuilder.append(", your magic number is: " ).append(magicNumber).toString());
+        return greeting;
+    }
+}
