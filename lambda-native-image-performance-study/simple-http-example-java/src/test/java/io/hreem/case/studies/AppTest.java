fff68782c374a60f4636c30e46e0a40e8109d074
Hassan Nazar <hassan.nazar@danielwellington.com>
2020-12-06 01:51:55 +0100

    Base setup complete

---
+++ lambda-native-image-performance-study/simple-http-example-java/src/test/java/io/hreem/case/studies/AppTest.java
@@ -0,0 +1,20 @@
+package io.hreem.case.studies;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
